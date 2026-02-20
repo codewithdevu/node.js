c27f664ff4998e83256b7441874ed33969c73e1a
codewithdevu <divyanshyadav8302@gmail.com>
2026-02-21 00:18:47 +0530

    Add

--- shortUrl-project/index.js
+++ shortUrl-project/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const urlRoute = require("./routes/url")
 const { connectToMongoDb } = require("./connection")
 const URL = require("./models/url")
+const path = require("path")
 
 const app = express();
 const PORT = 5001;
@@ -13,13 +14,26 @@ connectToMongoDb("mongodb://Localhost:27017/short-url")
 
     })
 
+
+
 //middleware
 app.use(express.json());
+
+// ejs setup
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.get("/test", async (req, res) => {
+    const allurls = await URL.find({});
+    return res.render("home", {
+        urls: allurls,
+    })
+})
 
 //route
 app.use("/url", urlRoute)
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
     const shortUrlId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortUrlId,
