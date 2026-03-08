d4dd7f9556b1fb4e3dfbceda48435778b244bf0e
codewithdevu <divyanshyadav8302@gmail.com>
2026-03-08 11:42:10 +0530

    add

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,8 +1,24 @@
 import express from "express";
 import http from "http";
+import path from "path";
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+
+const io = new Server(server);
+
+//socket io
+io.on("connection" , (socket) => {
+    console.log("a new user is connectod socket id:" , socket.id);
+})
+
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/" , (req , res ) => {
+    return res.sendFile("/public/index.html")
+})
 
 server.listen(5002 , () => {
     console.log(`server started at PORT http://localhost:${5002}`);
